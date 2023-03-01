728722839e20dc23475f9d7e99da6b2f96521efe
slexicon <lexiscott0101@gmail.com>
2023-02-28 19:54:26 -0800

    add procfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const api = require('./routes/notes.js');
 const html = require('./routes/html.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
