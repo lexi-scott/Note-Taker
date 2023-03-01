2be429d391c3bc4fb12df793bf3e77c374c177d7
slexicon <lexiscott0101@gmail.com>
2023-02-28 18:51:35 -0800

    fix bugs

--- routes/notes.js
+++ routes/notes.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
-// GET Route for retrieving all the notes
 router.get('/notes', (req, res) => {
   console.log("test route")
-  readFromFile('../db/db.json').then((data) => {
+  readFromFile('./db/db.json').then((data) => {
         console.log(JSON.parse(data));
         res.json(JSON.parse(data));
        
